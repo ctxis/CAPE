4237bedce445cd7848eca76bbde2f3b3415d5085
kevross33 <kevross33@gmail.com>
2018-02-11 13:51:59 +0000

    Remote code from office macros sig to split out

--- modules/signatures/office_macro.py
+++ modules/signatures/office_macro.py
@@ -22,10 +22,10 @@ from lib.cuckoo.common.abstracts import Signature
 
 class Office_Macro(Signature):
     name = "office_macro"
-    description = "The office file has a macro."
+    description = "The office file contains a macro"
     severity = 2
     categories = ["office"]
-    authors = ["KillerInstinct","Kevin Ross"]
+    authors = ["KillerInstinct"]
     minimum = "1.3"
 
     def run(self):
@@ -73,66 +73,5 @@ class Office_Macro(Signature):
                 self.description += " The file also appears to have strings indicating common phishing lures."
                 for positive in positives:
                     self.data.append({"Lure": positive})
-
-        # Increase severity on office documents with suspicious characteristics
-        if ret and package != "xls" and "static" in self.results and "office" in self.results["static"]:
-            if "Metadata" in self.results["static"]["office"]:
-                if "SummaryInformation" in self.results["static"]["office"]["Metadata"]:
-                    words = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["num_words"]
-                    if words == "0" or words == "None":
-                        self.severity = 3
-                        self.weight += 2
-                        self.data.append({"content" : "The file appears to have no content."})
-                        
-        if ret and package != "xls" and "static" in self.results and "office" in self.results["static"]:
-            if "Metadata" in self.results["static"]["office"]:
-                if "SummaryInformation" in self.results["static"]["office"]["Metadata"]:
-                    pages = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["num_pages"]
-                    if pages == "0" or pages == "None":
-                        self.severity = 3
-                        self.weight += 2
-                        self.data.append({"no_pages" : "The file appears to have no pages potentially caused by it being malformed or intentionally corrupted"})
-
-        if ret and "static" in self.results and "office" in self.results["static"]:
-            if "Metadata" in self.results["static"]["office"]:
-                if "SummaryInformation" in self.results["static"]["office"]["Metadata"]:
-                    author = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["author"]
-                    lastauthor = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["last_saved_by"]
-                    known_authors = ["Alex","Microsoft Office","Adder"]
-                    numerical_author = re.compile("^[0-9]{1,}$")
-                    for known_authors in known_authors:
-                        if author == known_authors:
-                            self.severity = 3
-                            self.weight += 2
-                            self.data.append({"malicious_author" : "The file appears to have been created by a known fake author indicative of an automated document creation kit."})
-
-                    if numerical_author.match(author):
-                        self.severity = 3
-                        self.weight += 2
-                        self.data.append({"numerical_author" : "The file author is numerical rather than a word/name indicative of an automated document creation kit."})
-
-                    if re.search("[0-9]{1}", author) and re.search("[A-Z]{1}", author):
-                        if len(author) < 6 and re.match("^[a-zA-Z0-9]{1,5}$", author):
-                            self.severity = 3
-                            self.weight += 2
-                            self.data.append({"short_author_format" : "The file author is a short text string yet contains numerical and upper case characters indicative of an automated document creation kit."})
-                        if len(author) > 5 and re.match("^[a-zA-Z0-9].*[A-Z].*$", author):
-                           if re.match("^[a-zA-Z0-9]{6,}$", author):
-                               self.data.append({"author_format" : "The file author contains a mix of numerical and upper case characters in an unlikely pattern indicative of an automated document creation kit."})
-
-                    if numerical_author.match(lastauthor):
-                        self.severity = 3
-                        self.weight += 2
-                        self.data.append({"numerical_last_saved" : "The file was last saved by a numerical author rather than a word/name indicative of an automated document creation kit."})
-
-                    if re.search("[0-9]{1}", lastauthor) and re.search("[A-Z]{1}", lastauthor):
-                        if len(lastauthor) < 6 and re.match("^[a-zA-Z0-9]{1,5}$", lastauthor):
-                            self.severity = 3
-                            self.weight += 2
-                            self.data.append({"short_last_saved_format" : "The file was last saved by an author with short text string containing numerical and upper case characters indicative of an automated document creation kit."})
-
-                        if len(lastauthor) > 5 and re.match("^[a-zA-Z0-9].*[A-Z].*$", lastauthor):
-                           if re.match("^[a-zA-Z0-9]{6,}$", lastauthor):
-                               self.data.append({"last_saved_format" : "The file was last saved by an author containing a mix of numerical and upper case characters in an unlikely pattern indicative of an automated document creation kit."})
 
         return ret
