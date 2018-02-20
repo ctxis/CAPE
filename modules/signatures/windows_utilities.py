2448fe39c3136e3b24c5a386fd6fa0b3fb0c4427
kevross33 <kevross33@gmail.com>
2018-02-20 17:44:30 +0000

    Update windows_utilities.py

--- modules/signatures/windows_utilities.py
+++ modules/signatures/windows_utilities.py
@@ -79,7 +79,7 @@ class UsesWindowsUtilities(Signature):
         for cmdline in cmdlines:
             lower = cmdline.lower()
             for utility in utilities:
-                if lower.startswith(utility):
+                if utility in lower:
                     ret = True
                     self.data.append({"command" : cmdline})
 
