e2070d0674954aae48f8a69c05f5a346b13cefd8
doomedraven <doomedraven@users.noreply.github.com>
2018-07-26 21:18:55 +0200

    simplify

--- modules/processing/curtain.py
+++ modules/processing/curtain.py
@@ -406,9 +406,7 @@ class Curtain(Processing):
                 if task in messages_by_task:
                     messages_by_task[task]["message"] = MESSAGE + messages_by_task[task]["message"]
                 else:
-                    messages_by_task.setdefault(task, dict())
-                    messages_by_task[task].setdefault("message", MESSAGE)
-                    messages_by_task[task].setdefault("pid", PID)
+                    messages_by_task.setdefault(task, dict()).update({"message": MESSAGE, "pid": PID})
 
         new_dict = [block_dict for block_dict in messages_by_task.values()]
 
