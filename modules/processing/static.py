7b1bc74cac58f4feeb85022eec160a61bf60149f
doomedraven <doommedraven@gmail.com>
2018-10-21 23:00:41 +0200

    code cleanup and fixes

--- modules/processing/static.py
+++ modules/processing/static.py
@@ -1115,80 +1115,51 @@ class Office(object):
         if rtfp.objects and not os.path.exists(save_dir):
             os.makedirs(save_dir)
         for rtfobj in rtfp.objects:
-            #ole_column = None
-            if rtfobj.is_ole:
-                results.setdefault(str(rtfobj.format_id), dict())
-                #ole_column = 'format_id: %d ' % rtfobj.format_id
-                if rtfobj.format_id == oleobj.OleObject.TYPE_EMBEDDED:
-                    results[str(rtfobj.format_id)].setdefault("type_embed", "Embedded")
-                elif rtfobj.format_id == oleobj.OleObject.TYPE_LINKED:
-                    results[str(rtfobj.format_id)].setdefault("type_embed", "Linked")
-                else:
-                    results[str(rtfobj.format_id)].setdefault("type_embed", "Unknown")
-                """
-                ole_column += 'class name: %r\n' % rtfobj.class_name
-                # if the object is linked and not embedded, data_size=None:
-                if rtfobj.oledata_size is None:
-                    ole_column += 'data size: N/A'
-                else:
-                    ole_column += 'data size: %d' % rtfobj.oledata_size
-                if rtfobj.is_package:
-                    ole_column += '\nOLE Package object:'
-                    ole_column += '\nFilename: %r' % rtfobj.filename
-                    ole_column += '\nSource path: %r' % rtfobj.src_path
-                    ole_column += '\nTemp path = %r' % rtfobj.temp_path
-                     check if the file extension is executable:
-                    _, ext = os.path.splitext(rtfobj.filename)
-                    log.debug('File extension: %r' % ext)
-                    if self.re_executable_extensions.match(ext):
-                        ole_column += '\nEXECUTABLE FILE'
-                else:
-                     pkg_column = 'Not an OLE Package'
-                """
-                if rtfobj.clsid is not None:
-                    #ole_column += '\nCLSID: %s' % rtfobj.clsid
-                    #ole_column += '\n%s' % rtfobj.clsid_desc
-                    if "CVE" in rtfobj.clsid_desc:
-                        results[str(rtfobj.format_id)].setdefault("CVE", rtfobj.clsid_desc)
-                # Detect OLE2Link exploit
-                # http://www.kb.cert.org/vuls/id/921560
-                if rtfobj.class_name == b'OLE2Link':
-                    #ole_column += '\nPossibly an exploit for the OLE2Link vulnerability (VU#921560, CVE-2017-0199)'
-                    results[str(rtfobj.format_id)].setdefault("CVE", "Possibly an exploit for the OLE2Link vulnerability (VU#921560, CVE-2017-0199)")
-            #else:
-                #ole_column = 'Not a well-formed OLE object'
-
-        for rtfobj in rtfp.objects:
-            i = rtfp.objects.index(rtfobj)
-                 # preset all keys
-                results[str(rtfobj.format_id)]["class_name"] = ""
-                results[str(rtfobj.format_id)]["size"] = ""
-                results[str(rtfobj.format_id)]["filename"] = ""
+            results.setdefault(str(rtfobj.format_id), dict())
+            results[str(rtfobj.format_id)]["class_name"] = ""
+            results[str(rtfobj.format_id)]["size"] = ""
+            results[str(rtfobj.format_id)]["filename"] = ""
+            results[str(rtfobj.format_id)]["type_embed"] = ""
+            results[str(rtfobj.format_id)]["CVE"] = ""
             if rtfobj.is_package:
-                log.debug('Saving file from OLE Package in object #%d:' % i)
+                log.debug('Saving file from OLE Package in object #%d:' % rtfobj.format_id)
                 log.debug('  Filename = %r' % rtfobj.filename)
                 log.debug('  Source path = %r' % rtfobj.src_path)
                 log.debug('  Temp path = %r' % rtfobj.temp_path)
-
                 if rtfobj.filename:
                     fname = convert_to_printable(rtfobj.filename)
                 else:
                     fname = 'object_%08X.noname' % rtfobj.start
                 log.debug('  saving to file %s' % fname)
-                results[str(rtfobj.format_id)].setdefault("filename", fname)
+                results[str(rtfobj.format_id)]["filename"] = fname
                 save_path = os.path.join(save_dir, fname)
                 open(save_path, 'wb').write(rtfobj.olepkgdata)
-                results[str(rtfobj.format_id)].setdefault("source_path", convert_to_printable(rtfobj.src_path))
+                #results[str(rtfobj.format_id)].setdefault("source_path", convert_to_printable(rtfobj.src_path))
             # When format_id=TYPE_LINKED, oledata_size=None
             elif rtfobj.is_ole and rtfobj.oledata_size is not None:
-                log.debug('Saving file embedded in OLE object #%d:' % i)
+                #ole_column = 'format_id: %d ' % rtfobj.format_id
+                if rtfobj.format_id == oleobj.OleObject.TYPE_EMBEDDED:
+                    results[str(rtfobj.format_id)]["type_embed"] = "Embedded"
+                elif rtfobj.format_id == oleobj.OleObject.TYPE_LINKED:
+                    results[str(rtfobj.format_id)]["type_embed"] = "Linked"
+                else:
+                    results[str(rtfobj.format_id)]["type_embed"] = "Unknown"
+                if rtfobj.clsid is not None:
+                    #ole_column += '\nCLSID: %s' % rtfobj.clsid
+                    #ole_column += '\n%s' % rtfobj.clsid_desc
+                    if "CVE" in rtfobj.clsid_desc:
+                        results[str(rtfobj.format_id)]["CVE"] = rtfobj.clsid_desc
+                # Detect OLE2Link exploit
+                # http://www.kb.cert.org/vuls/id/921560
+                if rtfobj.class_name == b'OLE2Link':
+                    #ole_column += '\nPossibly an exploit for the OLE2Link vulnerability (VU#921560, CVE-2017-0199)'
+                    results[str(rtfobj.format_id)].setdefault("CVE", "Possibly an exploit for the OLE2Link vulnerability (VU#921560, CVE-2017-0199)")
+                log.debug('Saving file embedded in OLE object #%d:' % rtfobj.format_id)
                 log.debug('  format_id  = %d' % rtfobj.format_id)
                 log.debug('  class name = %r' % rtfobj.class_name)
                 log.debug('  data size  = %d' % rtfobj.oledata_size)
-                results[str(rtfobj.format_id)].setdefault("format_id", rtfobj.format_id)
-                results[str(rtfobj.format_id)].setdefault("class_name", rtfobj.class_name)
-                results[str(rtfobj.format_id)].setdefault("size", rtfobj.oledata_size)
-
+                results[str(rtfobj.format_id)]["class_name"] = rtfobj.class_name
+                results[str(rtfobj.format_id)]["size"] = rtfobj.oledata_size
                 # set a file extension according to the class name:
                 class_name = rtfobj.class_name.lower()
                 if class_name.startswith(b'word'):
@@ -1198,19 +1169,20 @@ class Office(object):
                 else:
                     ext = 'bin'
                 fname = 'object_%08X.%s' % (rtfobj.start, ext)
-                results[str(rtfobj.format_id)].setdefault("filename", fname)
+                results[str(rtfobj.format_id)]["filename"] = fname
                 save_path = os.path.join(save_dir, fname)
                 log.debug('  saving to file %s' % fname)
                 open(save_path, 'wb').write(rtfobj.oledata)
-                results[str(rtfobj.format_id)].setdefault("filename", fname)
+                results[str(rtfobj.format_id)]["filename"] = fname
             else:
-                log.debug('Saving raw data in object #%d:' % i)
+                log.debug('Saving raw data in object #%d:' % rtfobj.format_id)
                 fname = 'object_%08X.raw' % rtfobj.start
-                results[str(rtfobj.format_id)].setdefault("filename", fname)
+                results[str(rtfobj.format_id)]["filename"] = fname
                 save_path = os.path.join(save_dir, fname)
                 log.debug('  saving object to file %s' % fname)
                 open(save_path, 'wb').write(rtfobj.rawdata)
-            results[str(rtfobj.format_id)].setdefault("index", "%08Xh" % rtfobj.start)
+            results[str(rtfobj.format_id)]["index"] = "%08Xh" % rtfobj.start
+        log.info(results)
         return results
 
     def _parse(self, filepath):
