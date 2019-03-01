85fd2f30bfddebf6db508c362f336ba0b58005b6
kevross33 <kevross33@gmail.com>
2019-03-01 14:17:12 +0000

    Update stealth child proc sig with extra info
    Sample with this behaviour https://cape.contextis.com/analysis/41482/

--- modules/signatures/stealth_childproc.py
+++ modules/signatures/stealth_childproc.py
@@ -8,17 +8,26 @@ class StealthChildProc(Signature):
     name = "stealth_childproc"
     description = "Forces a created process to be the child of an unrelated process"
     severity = 3
+    confidence = 100
     categories = ["stealth"]
     authors = ["Optiv"]
     minimum = "1.2"
     evented = True
+    references = "https://www.countercept.com/blog/detecting-parent-pid-spoofing/"
 
     def __init__(self, *args, **kwargs):
         Signature.__init__(self, *args, **kwargs)
+        self.ret = False
 
     filter_apinames = set(["NtCreateProcess","NtCreateProcessEx","RtlCreateUserProcess","CreateProcessInternalW"])
 
     def on_call(self, call, process):
         parenthandle = self.get_argument(call, "ParentHandle")
+        pname = process["process_name"].lower()
+        cmdline = self.get_argument(call, "CommandLine")
         if parenthandle and parenthandle != "0xffffffff" and parenthandle != "0xffffffffffffffff":
-            return True
+            self.ret = True
+            self.data.append({"created_process": "Process %s has spoofed parent process with real parent process %s" % (cmdline,pname)})
+
+    def on_complete(self):
+        return self.ret
