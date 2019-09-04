8c2741f44f3ec49ccaff574f473e5d97d64a5e5e
kevross33 <kevross33@gmail.com>
2019-09-04 12:13:28 +0100

    Add in some more UAC bypasses

--- modules/signatures/bypass_uac.py
+++ modules/signatures/bypass_uac.py
@@ -20,7 +20,7 @@ class UACBypassEventvwr(Signature):
     description = "Uses eventvwr technique to bypass User Access Control (UAC)"
     severity = 3
     confidence = 100
-    categories = ["uac"]
+    categories = ["uac_bypass"]
     authors = ["Kevin Ross"]
     minimum = "1.3"
     evented = True
@@ -54,4 +54,91 @@ class UACBypassEventvwr(Signature):
                     self.ret = True   
 
     def on_complete(self):
+        return self.ret
+
+class UACBypassDelegateExecuteSdclt(Signature):
+    name = "uac_bypass_delegateexecute_sdclt"
+    description = "Uses delegate execute sdclt technique to bypass User Access Control (UAC)"
+    severity = 3
+    confidence = 100
+    categories = ["uac_bypass"]
+    authors = ["Kevin Ross"]
+    minimum = "1.3"
+    evented = True
+    references = ["http://blog.sevagas.com/?Yet-another-sdclt-UAC-bypass"]
+    ttp = ["T1088"]
+
+    def run(self):
+        regkey = False
+        ret = False
+
+        keys = [
+            ".*\\\\Software\\\\Classes\\\\Folder\\\\shell\\\\open\\\\command\\\\DelegateExecute$",
+        ]
+
+        for check in keys:
+            match = self.check_write_key(pattern=check, regex=True)
+            if match:
+                self.data.append({"regkey" : match})
+                regkey = True
+
+        cmdlines = self.results["behavior"]["summary"]["executed_commands"]
+        for cmdline in cmdlines:
+            lower = cmdline.lower()
+            if regkey and "sdclt" in lower:
+                self.data.append({"cmdline" : cmdline})
+                ret = True
+
+        return ret
+
+class UACBypassCMSTP(Signature):
+    name = "uac_bypass_cmstp"
+    description = "Uses cmstp.exe sendkeys technique to bypass User Access Control (UAC)"
+    severity = 3
+    confidence = 100
+    categories = ["uac_bypass"]
+    authors = ["Kevin Ross"]
+    minimum = "1.3"
+    evented = True
+    references = ["https://oddvar.moe/2017/08/15/research-on-cmstp-exe/"]
+    ttp = ["T1088"]
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.inf = False
+        self.droppedinf = []
+        self.ret = False
+
+    filter_apinames = set(["CopyFileExA","CopyFileExW","MoveFileWithProgressW","MoveFileWithProgressTransactedW","NtWriteFile"])
+
+    def on_call(self, call, process):
+        # This is a straight catch of the .inf file with content we want being dropped
+        if call["api"] == "NtWriteFile":
+            filename = self.get_argument(call, "HandleName")
+            if filename.endswith(".inf"):
+                buf = self.get_argument(call, "Buffer")
+                if "runpresetupcommands" in buf.lower():
+                    self.data.append({"dropped .inf file": filename })
+                    self.droppedinf.append(filename)
+                    self.inf = True
+
+        # This is for a file being moved/renamed into .inf. This is to avoid a possible evasion that could be created by dropped the content in a .txt or something and then renaming the file/moving it into a .inf for use my cmstp. Also in case of copying .inf files into new ones too.        
+        if call["api"] in ("CopyFileExA","CopyFileExW","MoveFileWithProgressW","MoveFileWithProgressTransactedW"):
+            origfile = self.get_argument(call, "ExistingFileName")
+            destfile = self.get_argument(call, "NewFileName")
+            if destfile.endswith(".inf"):
+                self.data.append({"dropped .inf file" : "%s was moved to destination file %s" % (origfile,destfile)})
+                self.droppedinf.append(destfile)
+                self.inf = True
+
+    def on_complete(self):
+        cmdlines = self.results["behavior"]["summary"]["executed_commands"]
+        for cmdline in cmdlines:
+            lower = cmdline.lower()
+            if self.inf and "cmstp" in lower and ".inf" in lower:
+                for dropped in self.droppedinf:
+                    if dropped.lower() in lower:
+                        self.data.append({"cmdline" : cmdline})
+                        self.ret = True
+
         return self.ret
