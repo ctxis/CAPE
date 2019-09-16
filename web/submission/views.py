ed9ac663bbc6be1b2aa1b57111169d4e2a12db4b
unknown <kevoreilly@gmail.com>
2019-09-16 14:18:34 +0100

    Fix bad character in submission/views.py

--- web/submission/views.py
+++ web/submission/views.py
@@ -533,7 +533,7 @@ def index(request, resubmit_hash=False):
             enabledconf["tags"] = True
 
         if not enabledconf["tags"]:
-            # load multi machinery tags:
+            # load multi machinery tags:
             if machinery == "multi":
                 for mmachinery in Config(machinery).multi.get("machinery").split(","):
                     vms = [x.strip() for x in getattr(Config(mmachinery), mmachinery).get("machines").split(",")]
